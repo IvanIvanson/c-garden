944639decd09a075690d447a84516abf546cb0ec
IvanIvanson <ghalut1977@gmail.com>
2022-10-29 14:40:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+const menu = document.querySelector('.menu')
+const btnIconMenu = document.querySelector('.icon-menu')
+
+btnIconMenu.addEventListener('click', function(){
+    menu.classList.toggle('menu-show')
+  
+})
+
+
+}
