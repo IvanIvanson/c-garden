2f3328de39965979b22059fca0e4c9887d487477
IvanIvanson <ghalut1977@gmail.com>
2022-10-29 16:55:34 +0300

    fixed header

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-window.onload = function() {
+window.onload = function () {
 
-const menu = document.querySelector('.menu')
-const btnIconMenu = document.querySelector('.icon-menu')
+    const menu = document.querySelector('.menu')
+    const btnIconMenu = document.querySelector('.icon-menu')
 
-btnIconMenu.addEventListener('click', function(){
-    menu.classList.toggle('menu-show')
+    btnIconMenu.addEventListener('click', function () {
+        menu.classList.toggle('menu-show')
   
-})
+    });
 
+    let last_known_scroll_position = 0;
+    let ticking = 0;
+    function doScrol(scroll_pos) {
+    
+        if (window.innerWidth <= 770 && scroll_pos > 0) {
+            headerBottom.classList.add("shaddowHeader");
+        } else {
+            headerBottom.classList.remove("shaddowHeader");
+        }
+    }
+    let headerBottom = document.getElementById('header-bottom');
+    window.addEventListener('scroll', function (e) {
+        last_known_scroll_position = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(function () {
+                doScrol(last_known_scroll_position);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
 
 }
